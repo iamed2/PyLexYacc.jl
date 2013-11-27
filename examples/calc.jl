1521fbe5035a12ef95563a8232076f8d52cc74c2
Eric Davies <iamed2@gmail.com>
2013-11-27 16:01:42 -0600

    Changed example to be cleaner Module version.

--- examples/calc.jl
+++ examples/calc.jl
@@ -3,54 +3,61 @@
 using PLY
 
 
-tokrules = [
-	:tokens    => (
+module rules
+	using PLY
+
+	tokens = (
 		"NAME","NUMBER",
 		"PLUS","MINUS","TIMES","DIVIDE","EQUALS",
 		"LPAREN","RPAREN"
-		),
+		)
+
 	# Tokens
 
-	:t_PLUS    => "\\+",
-	:t_MINUS   => "-",
-	:t_TIMES   => "\\*",
-	:t_DIVIDE  => "/",
-	:t_EQUALS  => "=",
-	:t_LPAREN  => "\\(",
-	:t_RPAREN  => "\\)",
-	:t_NAME    => "[a-zA-Z_][a-zA-Z0-9_]*",
-	:t_NUMBER  => rule("\\d+") do t, lexer
+	t_PLUS    = "\\+"
+	t_MINUS   = "-"
+	t_TIMES   = "\\*"
+	t_DIVIDE  = "/"
+	t_EQUALS  = "="
+	t_LPAREN  = "\\("
+	t_RPAREN  = "\\)"
+	t_NAME    = "[a-zA-Z_][a-zA-Z0-9_]*"
+	
+	t_NUMBER = rule("\\d+") do t, lexer
 			t["value"] = int(t["value"])
 			return t
-		end,
-	:t_ignore  => " \t",
-	:t_newline => rule("[\\r\\n]+") do t, lexer
+		end
+
+	t_ignore  = " \t"
+
+	t_newline = rule("[\\r\\n]+") do t, lexer
 			lexer["lineno"] += count((x)->(x == '\n'),t["value"])
 			return nothing
-		end,
-	:t_error   => rule() do t, lexer
+		end
+
+	t_error = rule() do t, lexer
 			@printf("Illegal character '%s'\n", t["value"][1])
 			skip(lexer, 1)
 			return t
 		end
-]
 
-vars = Dict()  # symbol table
-parserules = [
-	:tokens              => tokrules[:tokens],
-	:precedence          => (
+	precedence = (
 			("left", "PLUS", "MINUS"),
 			("left", "TIMES", "DIVIDE"),
 			("right", "UMINUS")
-		),
-	# (:vars               , Dict()),
-	:p_statement_assign  => parserule("statement : NAME EQUALS expression") do t
+		)
+	
+	vars = Dict()  # symbol table
+
+	p_statement_assign = parserule("statement : NAME EQUALS expression") do t
 			vars[t[2]] = t[4]
-		end,
-	:p_statement_expr    => parserule("statement : expression") do t
+		end
+
+	p_statement_expr = parserule("statement : expression") do t
 			println(t[2])
-		end,
-	:p_expression_binop  => parserule(
+		end
+
+	p_expression_binop = parserule(
 			"""expression : expression PLUS expression
 			              | expression MINUS expression
 			              | expression TIMES expression
@@ -64,31 +71,36 @@ parserules = [
 			elseif t[3] == "/"
 				t[1] = t[2] / t[4]
 			end
-		end,
-	:p_expression_uminus => parserule("expression : MINUS expression %prec UMINUS") do t
+		end
+
+	p_expression_uminus = parserule("expression : MINUS expression %prec UMINUS") do t
 			t[1] = -t[3]
-		end,
-	:p_expression_group  => parserule("expression : LPAREN expression RPAREN") do t
+		end
+
+	p_expression_group = parserule("expression : LPAREN expression RPAREN") do t
 			t[1] = t[3]
-		end,
-	:p_expression_number => parserule("expression : NUMBER") do t
+		end
+
+	p_expression_number = parserule("expression : NUMBER") do t
 			t[1] = t[2]
-		end,
-	:p_expression_name   => parserule("expression : NAME") do t
+		end
+
+	p_expression_name = parserule("expression : NAME") do t
 			try
 				t[1] = vars[t[2]]
 			catch
 				@printf("Undefined name '%s'\n", t[2])
 				t[1] = 0
 			end
-		end,
-	:p_error             => rule() do t, lexer
+		end
+
+	p_error = rule() do t, lexer  # the error rule actually takes a LexToken so make it a rule()
 			@printf("Syntax error at '%s'\n", t["value"][1])
 		end
-]
+end
 
 l = lexer(tokrules)
-p = parser(parserules, "statement")  # specifiy start rule here or through :start key in dict
+p = parser(parserules, "statement")  # specifiy start rule here or through start variable in module
 
 # replicates the input function using in calc.py
 function input(str::String)
